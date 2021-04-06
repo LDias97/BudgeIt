711546f789298381caf592c5078c62775f7c7b84
Ben <benviscosi@gmail.com>
2021-04-06 14:28:08 -0400

    networthvm

--- Rev0/Models/NetWorthViewModel.swift
+++ Rev0/Models/NetWorthViewModel.swift
@@ -4,7 +4,6 @@ import FirebaseFunctions
 final class NetWorthViewModel: ObservableObject {
     
     @Published var currentBalance: Double = 0.0
-    @Published var previousBalance: Double = UserDefaults.standard.double(forKey: "lastMonthBalance")
     @Published var difference: Double = 0.0
     @Published var spent: Double = 0.0
     @Published var earned: Double = 0.0
@@ -14,13 +13,8 @@ final class NetWorthViewModel: ObservableObject {
         PlaidAPI().getBalance(){ (netWorth) in
             DispatchQueue.main.async {
                 self.currentBalance = netWorth
-                self.difference = self.currentBalance - self.previousBalance
-                if (self.difference > 0) {
-                    self.earned = self.difference
-                }
-                else {
-                    self.spent = self.difference
-                }
+                self.earned = netWorth
+                self.difference = self.earned - self.spent
                 self.isLoading = false
                 
             }
