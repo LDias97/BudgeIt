b25617e97c1ba5749f258c88a308f121cdf447f3
Ben <benviscosi@gmail.com>
2021-04-07 12:57:53 -0400

    fix backwards ternary op

--- Rev0/Navigation/ViewRouter.swift
+++ Rev0/Navigation/ViewRouter.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 final class ViewRouter: ObservableObject {
-    @Published var currentPage: Page =  UserDefaults.standard.value(forKey: "logged_in")! as! Bool ? .page2 : .page3
+    @Published var currentPage: Page =  UserDefaults.standard.value(forKey: "logged_in")! as! Bool ? .page3 : .page2
 }
